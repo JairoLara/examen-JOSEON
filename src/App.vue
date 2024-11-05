9790285e870c89b4c9fde2e21fff7cce19701406
Jordix69 <laracisnerosjairo@gmail.com>
2024-11-05 08:14:30 -0600

    joseon

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/NavBar.vue'
+</script>
+
+<template>
+  <div class="ps-8 pe-10">
+    <Navbar />
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import Footer1 from '../components/Footer.vue'
+import inicioTop from '@/components/inicioTop.vue'
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <inicioTop></inicioTop>
+  <Footer1></Footer1>
+</template>
+
+<style scoped>
+#nav {
+  top: 0%;
+  width: 100vw;
+  padding: 10px;
+  box-sizing: border-box;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.row {
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.col {
+  flex: 1;
+  padding: 20px;
+  margin-right: 10px;
+  text-align: center;
+}
+
+.col:last-child {
+  margin-right: 0;
+}
+</style>
